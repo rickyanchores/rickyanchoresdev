158e0d1f0cf0c3bb8c691c81281ada62c521642c
RickyAnchores <ricky23anchores@gmail.com>
2022-06-14 20:17:11 +0100

    commit component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <h1>Ricky Portfolio v3.0</h1>
+      <Home />
     </div>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='Home'>
+            <h1>Home</h1>
+            <p>this is home section</p>
+        </div>
+    )
+}
+
+export default Home;
+    
