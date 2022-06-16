6eabe6f61a9e3cb51b5831c224ee0130d6436637
RickyAnchores <ricky23anchores@gmail.com>
2022-06-16 13:42:15 +0100

    map nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Footer from './pages/Footer/Footer';
 import Home from './pages/Home/Home';
 
 const App = () => {
   return (
-    <div className='App'>
+    <Router>
       <Nav />
-      <h1>Ricky Portfolio v3.0</h1>
+      <Routes>
+        <Route component={Home} />   
+      </Routes>
       <Home />
       <Footer />
-    </div>
+    </Router>
   )
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import { NavLink as Link } from 'react-router-dom'
 
 const Nav = () => {
+
+    const navList = [
+        {
+            name: "Home",
+            href: "/"
+        },
+        {
+            name: "About",
+            href: "/"
+        },
+        {
+            name: "FAQ",
+            href: "/"
+        }
+    ]
+
+
     return (
         <div className='Nav'>
             <div className="logo">RA</div>
             <div className="hamburger">X</div>
             <div className="navLinks">
-                <li>Home</li>
-                <li>About</li>
-                <li>FAQ</li>
+               {navList.map((link) => {
+                   return(
+                       <Link to={link.href}>{link.name}</Link>
+                   )
+               })}
             </div>
         </div>
     )
