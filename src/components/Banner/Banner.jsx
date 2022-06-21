7f36eb00a3900c2c0daf3875734a0a4b10cda3cf
RickyAnchores <ricky23anchores@gmail.com>
2022-06-21 18:56:17 +0100

    edit banner and border

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div className='Bannner'>
+        <div className='Banner'>
             <h1>Banner</h1>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae autem earum iure, ea est rem?</p>
         </div>
