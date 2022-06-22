9e79b987059e4f227b810cf3a20195414518cac4
RickyAnchores <ricky23anchores@gmail.com>
2022-06-22 20:25:59 +0100

    added home details

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,8 @@ const Home = () => {
         <div className='Home'>
             <div className="title">
             <h1>Hi, I'm Ricky</h1>
-            <p>this is home section</p>
+            <h2>Front End Developer</h2>
+            <h4>HTML / CSS / Javascript / React</h4>
             <Button />
             </div>
             <img src="https://scontent.fltn2-1.fna.fbcdn.net/v/t39.30808-6/284985848_10221559855690034_4547629195284456493_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=J4_zoGCZXfQAX9T6t0x&_nc_ht=scontent.fltn2-1.fna&oh=00_AT9fib5h6I1MmsaOPiWHw7RJSouFIgrvcftYkzITJH7rmQ&oe=62AF8BCE" alt="me" />
