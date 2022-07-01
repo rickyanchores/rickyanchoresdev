bdfa5e4217bf62dac4d4c8b77f338d9f96bb4114
RickyAnchores <ricky23anchores@gmail.com>
2022-07-01 13:03:43 +0100

    added HeroImage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Button from '../../components/Button/Button';
 
+import HeroImage from "../../images/Ricky.jpg";
+
 const Home = () => {
 
     return (
@@ -11,8 +13,7 @@ const Home = () => {
             <h4>HTML / CSS / Javascript / React</h4>
             <Button />
             </div>
-            <img src="https://scontent.fltn2-1.fna.fbcdn.net/v/t39.30808-6/284985848_10221559855690034_4547629195284456493_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=J4_zoGCZXfQAX9T6t0x&_nc_ht=scontent.fltn2-1.fna&oh=00_AT9fib5h6I1MmsaOPiWHw7RJSouFIgrvcftYkzITJH7rmQ&oe=62AF8BCE" alt="me" />
-
+            <img src={HeroImage} alt="me" />
         </div>
     )
 }
