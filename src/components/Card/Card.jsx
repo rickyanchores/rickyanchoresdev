4b708e57b9b5395d27391f67acd14e3cf72e24df
RickyAnchores <ricky23anchores@gmail.com>
2022-07-04 22:48:45 +0100

    update project section

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Card = (card) => {
     return (
         <div className='Card'>
+            <img src={card.image} alt="" />
             <h3>{card.title}</h3>
             <p>{card.description}</p>
+            <div className="btn-container">
+                <button className='btn'>VIEW</button>
+                <button className='btn'>LIVE</button>
+            </div>
         </div>
     )
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Card from '../../components/Card/Card'
 
+// IMPORT IMAGES
+import imageOne from "../../images/Citypx.jpg";
+import imageTwo from "../../images/kahoii.jpg";
+import imageThree from "../../images/zenkana.jpg";
+
+
 const Projects = (props) => {
 
     const card = [
-        {
+        {   
             title: "Project One",
             description: "This is project one"
         },
@@ -22,12 +28,12 @@ const Projects = (props) => {
         <div className='Projects'>
             <h3>Projects</h3>
             <div className="proj-container">
-                <Card title={card[0].title} description={card[0].description}/>
-                <Card title={card[1].title} description={card[1].description}/>
-                <Card title={card[2].title} description={card[2].description}/>
+                <Card image={imageOne} title={card[0].title} description={card[0].description}/>
+                <Card image={imageTwo} title={card[1].title} description={card[1].description}/>
+                <Card image={imageThree} title={card[2].title} description={card[2].description}/>
             </div>
         </div>
     )
 }
 
-export default Projects
+export default Projects;
