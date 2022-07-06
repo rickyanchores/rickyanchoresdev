ac8f566e1d36bda915e645fe65dcaded1f8b228e
RickyAnchores <ricky23anchores@gmail.com>
2022-07-06 21:07:44 +0100

    update footer section

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -4,6 +4,18 @@ const Footer = () => {
     return (
         <div className='Footer'>
             <h1>Footer</h1>
+            <div className="container">
+                <div className="left">
+                    <h3>GET IN TOUCH:</h3>
+                    <h5>Email: info@mysite.com</h5>
+                    <h5>London, United Kingdom</h5>
+                    <h5>&copy;2023 Developed by Ricky A</h5>
+                </div>
+                <div className="right">
+                    <h3>CONTACT US:</h3>
+                    
+                </div>
+            </div>
         </div>
     )
 }
