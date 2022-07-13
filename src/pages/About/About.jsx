a6cb775353ebca9786487026b2c3cb7cc80cf32d
RickyAnchores <ricky23anchores@gmail.com>
2022-07-13 20:50:49 +0100

    added about text

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,7 +5,10 @@ const About = () => {
         <div className='About' id='about'>
             <div className="container">
                 <img src="https://rickyanchores.github.io/portfoliov2/static/media/me-back.ddf4cec7.jpg" alt="" />
+                <div className="abt-left">
                 <h3>About</h3>
+                <p>I'm Ricky, a Front End Developer based in London,UK Web Dev and Design. Well organised person, problem solver, independent employee with high attention to detail. Music and Art passionate</p>
+                </div>
             </div>
         </div>
     )
