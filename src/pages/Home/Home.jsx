9f942e7b15faf56fe4fccec7630f5c7535bb980f
RickyAnchores <ricky23anchores@gmail.com>
2022-07-18 22:46:49 +0100

    card edit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,9 +11,9 @@ const Home = () => {
                 <img src={HeroImage} alt="me" />
             </div>
             <div className="title">
-            <h1>Hi, I'm Ricky</h1>
-            <h2>Front End Developer</h2>
-            <h4>HTML / CSS / Javascript / React</h4>
+                <h1>Hi, I'm Ricky</h1>
+                <h2>Front End Developer</h2>
+                <h4>HTML / CSS / Javascript / React</h4>
             <button className='hero-btn'>Download CV</button>
             </div>
         </div>
