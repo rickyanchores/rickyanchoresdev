775b435ccaf34299969b1cb68a80e24963dd650b
RickyAnchores <ricky23anchores@gmail.com>
2022-07-26 20:12:50 +0100

    added navbar mui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Banner from './components/Banner/Banner';
 import Nav from './components/Nav/Nav';
+import Navbar from './components/Navbar/Navbar';
 import About from './pages/About/About';
 import Footer from './pages/Footer/Footer';
 import Home from './pages/Home/Home';
@@ -10,7 +11,8 @@ import Projects from './pages/Projects/Projects';
 const App = () => {
   return (
     <Router>
-      <Nav />
+
+      <Navbar />
       <Routes>
         <Route component={Home} />   
       </Routes>
