b95150fec7a2f9a837ac9a5d1cc5ce70a776a4c2
RickyAnchores <ricky23anchores@gmail.com>
2022-08-06 21:29:51 +0100

    added social links to Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import { FaFacebook, FaInstagram,FaGithub  } from "react-icons/fa"
 import HeroImage from "../../images/Riko.jpeg";
 
 const Home = () => {
 
+
+    const iconStyle = {
+        color: "black",
+        fontSize: "2em",
+        margin: "0.5"
+    }
 
     return (
         <div className='Home' id='/'>
@@ -14,6 +20,17 @@ const Home = () => {
                 <h1>Hi, I'm Ricky</h1>
                 <h2>Front End Developer</h2>
                 <h4>HTML / CSS / Javascript / React</h4>
+                <div className="sc-container">
+                <div className="socialLink">
+                        <FaFacebook style={iconStyle}/>
+                    </div>
+                    <div className="socialLink">
+                        <FaInstagram style={iconStyle}/>
+                    </div>
+                    <div className="socialLink">
+                        <FaGithub style={iconStyle}/>
+                    </div>
+                </div>
             <button className='hero-btn'>Download CV</button>
             </div>
             
