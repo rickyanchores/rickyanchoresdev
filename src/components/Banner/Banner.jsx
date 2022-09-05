5c3429e8d0051f7f4db1922dfc74d9732cbe1c8f
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2022-09-05 18:35:38 +0100

    edit banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import SocialMedia from "../SocialMedia/SocialMedia";
 
 const Banner = () => {
     return (
         <div className='Banner'>
             <div className="container">
                 <h3>Banner</h3>
-                <SocialMedia />
+
             </div>
         </div>
     )
