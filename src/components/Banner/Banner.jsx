634d8ea6c981bd4bc605cd70f79c4c2c6ae5cbfe
RickyAnchores <ricky23anchores@gmail.com>
2022-12-10 22:02:33 +0000

    edit header name

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div className='Banner'>
+        <div className='Banner' data-aos="fade-right">
             <div className="container">
                 
             </div>
