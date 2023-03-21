7075777c795770e0e6b9be2face7c2701aee7904
RickyAnchores <ricky23anchores@gmail.com>
2023-03-21 18:53:20 +0000

    edit about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
                 <img src="https://rickyanchores.github.io/portfoliov2/static/media/me-back.ddf4cec7.jpg" alt="" />
                 <div className="abt-left">
                 <h3 data-aos="fade-left">About</h3>
-                <p>I'm Ricky, a Front End Developer based in London,UK Web Dev and Design. Well organised person, problem solver, independent employee with high attention to detail. Music and Art passionate</p>
+                <p>I am a front-end developer with 3 years of experience in HTML, CSS, JavaScript, and React. My passion lies in creating visually appealing and responsive websites that are both functional and user-friendly. I specialize in using HTML and CSS to design beautiful user interfaces, while also leveraging JavaScript to add interactivity.I enjoy problem-solving and keeping up-to-date with the latest trends in web development. I am excited to work with you and create something amazing.</p>
                 </div>
             </div>
         </div>
