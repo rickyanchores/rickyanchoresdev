d8db456f8253f4a7c5151753352aa32de7a756b0
RickyAnchores <ricky23anchores@gmail.com>
2023-04-19 20:38:53 +0100

    added card techstack icons

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaHtml5, FaCss3, FaJs, FaSass } from "react-icons/fa"
+
+
 
 const Card = (card) => {
     return (
@@ -6,6 +9,12 @@ const Card = (card) => {
             <img src={card.image} alt="" />
             <h3>{card.title}</h3>
             <p>{card.description}</p>
+            <div className="tech-stack">
+               <i><FaHtml5></FaHtml5></i>
+               <i><FaCss3></FaCss3></i>
+               <i><FaJs></FaJs></i>
+               <i><FaSass></FaSass></i>
+            </div>
             <div className="btn-container">
                 <button className='btn'>
                     <a href={card.code} target="_blank" rel='noreferrer'>CODE</a>
