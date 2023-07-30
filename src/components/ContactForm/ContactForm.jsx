5ef93ec89b051ff4fa55f6d24958a431b304f08e
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-07-30 15:37:53 +0100

    added contact form

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can add code here to handle form submission (e.g., send form data to an API)
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="ContactForm">
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default ContactForm;
