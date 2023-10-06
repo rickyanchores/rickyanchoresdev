fd80de2b1422b242154773572247b22630a91bc3
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2023-10-06 23:50:18 +0100

    edit banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import wave from "../../images/wave.svg";
 const Banner = () => {
     return (
         <div className='Banner' data-aos="fade-right">
+            <img src={wave} alt="" />
         </div>
     )
 }
